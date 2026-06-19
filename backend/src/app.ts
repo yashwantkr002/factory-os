37e1eebc861e14a659d375ed96838445b6551ac7
Yashwant Das <yashwantkr002gmail.com>
2026-06-19 10:29:13 +0530

    add

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import authRoutes from "./modules/auth/auth.routes.ts";
 import productRoutes from "./modules/products/product.routes.js";
+import vendorRoutes from "./modules/vendors/vendors.routes.js";
 
 const app = express();
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/vendors", vendorRoutes);
 
 app.get("/", (req, res) => {
   res.json({
